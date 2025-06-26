34d163e725dca9edc3f902e4c48fd60c6a709b04
Hossain Uddin <hossainuddinahamad@gmail.com>
2025-06-26 07:00:21 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        // Add your custom color here
+        "custom-dark": "#1D2C47",
+        "custom-main": "#EF3D3D",
+        "custom-text": "#4C4C4C",
+      },
+    },
+  },
+  plugins: [],
+};
